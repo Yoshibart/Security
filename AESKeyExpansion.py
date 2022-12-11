20f3324389a10141e7c5b5b82606a25fdce1f9fb
Barnet Obeka <oworibarnet@gmail.com>
2022-12-11 17:11:06 +0000

    Created the basics components need the rotword,subword,RCon

--- AESKeyExpansion.py
+++ AESKeyExpansion.py
@@ -1,11 +1,18 @@
 def keyExpansion(key):
 	keys = []
-	tempKey = []
 	counter = 0
 
 	while(counter < len(key)):
 		keys.append([key[counter:counter+8]])
 		counter+=8
+
+	for i in range(4, 44):
+		tempKey = keys[-1]
+		if i % 4 == 0:
+			tempKey = rotWord(tempKey)
+			tempKey = subWord(tempKey)
+			tempKey = rCon(tempKey)
+		keys[i] = XOR(keys[i - 4], tempKey)
 	return keys
 
 key = "0f1571c947d9e8590cb7add6af7f6798"
