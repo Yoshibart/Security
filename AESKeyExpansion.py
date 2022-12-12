6e2453d59ccd8459278b84d3195379c01af2f4c8
Barnet Obeka <oworibarnet@gmail.com>
2022-12-12 13:10:33 +0000

    Created the subWord() that uses sBox() for substitution

--- AESKeyExpansion.py
+++ AESKeyExpansion.py
@@ -30,6 +30,19 @@ def sBox():
 		for j in range(0, 16):
 			sBox[str(i) + "," + str(j)] = Box[i][j]
 	return sBox
+	
+def subWord(tempKey):
+	subTemp = ""
+	Box = sBox()
+	letters = {"a":"10", "b":"11", "c":"12", "d":"13", "e":"14", "f":"15",
+				"0":"0", "1":"1", "2":"2", "3":"3", "4":"4", "5":"5", "6":"6",
+				"7":"7", "8":"8", "9":"9"}
+	for i in range(0,len(tempKey),2):
+		f = tempKey[i:i+2]
+		key = letters[f[0:1]]+','+letters[f[1:2]]
+		subTemp += Box[key]
+
+	return subTemp
 
 for i in keyExpansion(key):
 	print(i)
