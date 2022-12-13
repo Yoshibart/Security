0f630a2123f3bdbdfa9f4823c4c8bb41e9d7f548
Barnet Obeka <oworibarnet@gmail.com>
2022-12-13 11:39:12 +0000

    Cipher Text Frequency distribution using pandas

---
+++ monoalphabetic.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def monoalpabetic(letters):
+	letters = letters.upper()
+	frequency = {"E":12.7,"T":9.1,"A":8.2,"O":7.5,"I":7.0,"N":6.7,"S":6.3,"H":6.1,"R":6.0,
+	"D":4.3,"L":4.0,"C":2.8,"U":2.8,"M":2.4,"W":2.4,"F":2.2,"G":2.0,"Y":2.0,"P":1.9,"B":1.5,
+	"V":1.0,"K":0.8,"J":0.2,"X":0.2,"Q":0.1,"Z":0.1}
+	occur = {}
+	for i in letters:
+		if i not in occur:
+			occur[i] = 1
+		else:
+			occur[i]+=1
+	for i in occur.keys():
+		occur[i] = round((occur[i] / len(letters)) * 100, 2)
+		
+	occur = createSeries(occur)
+	return occur
+
+def createSeries(letters):
+ 	return pd.Series(letters,name = "freq").sort_values(ascending=False)
+
+cipher = "UZQSOVUOHXMOPVGPOZPEVSGZWSZOPFPESXUDBMETSXAIZVUEPHZHMDZSHZOWSFPAPPDTSVPQUZWYMXUZUHSXEPYEPOPDZSZUFPOMBZWPFUPZHMDJUDTMOHMQ"
+print(monoalpabetic(cipher))
